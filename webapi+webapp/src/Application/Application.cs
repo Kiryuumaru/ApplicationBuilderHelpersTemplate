302a34d54251f4afbc5cf8e3c8ef79c0329b91ac
Clynt Neiko Rupinta <neiko@meldcx.com>
2026-01-19 20:12:10 +0800

    Set ASPNETCORE_ENVIRONMENT based on build configuration
    Add RunPreparation override to Application to set the ASPNETCORE_ENVIRONMENT variable and update configuration to "Development" or "Production" depending on build type. Also add missing using directive for AppEnvironment.Services.

--- webapi+webapp/src/Application/Application.cs
+++ webapi+webapp/src/Application/Application.cs
@@ -1,9 +1,11 @@
 using Application.AppEnvironment.Extensions;
+using Application.AppEnvironment.Services;
 using Application.Common.Extensions;
 using Application.LocalStore.Extensions;
 using Application.Logger.Extensions;
 using Application.NativeCmd.Extensions;
 using ApplicationBuilderHelpers;
+using Domain.AppEnvironment.Constants;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,5 +22,18 @@ public class Application : ApplicationDependency
         services.AddAppEnvironmentServices();
         services.AddNativeCmdServices();
         services.AddLocalStoreServices();
+    }
+
+    public override void RunPreparation(ApplicationHost applicationHost)
+    {
+        base.RunPreparation(applicationHost);
+
+#if DEBUG
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+        applicationHost.Configuration["ASPNETCORE_ENVIRONMENT"] = "Development";
+#else
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Production");
+        applicationHost.Configuration["ASPNETCORE_ENVIRONMENT"] = "Production";
+#endif
     }
 }
